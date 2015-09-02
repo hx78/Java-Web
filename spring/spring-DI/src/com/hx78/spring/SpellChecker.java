a1eed1a84a191ce78b796e1dfa9699e4fef292bc
hx78 <hangxing78@163.com>
2015-09-02 14:16:21 +0800

    修改spring-DI

--- spring/spring-DI/src/com/hx78/spring/SpellChecker.java
+++ spring/spring-DI/src/com/hx78/spring/SpellChecker.java
@@ -1,8 +1,11 @@
 package com.hx78.spring;
 
+import org.springframework.stereotype.Service;
+
 /**
  * Created by linming on 2015/8/31 0031.
  */
+//@Service("spellChecker")
 public class SpellChecker {
     public SpellChecker() {
         System.out.println("Inside SpellChecker constructor." );
--- spring/spring-DI/src/com/hx78/spring/TextEditor.java
+++ spring/spring-DI/src/com/hx78/spring/TextEditor.java
@@ -2,23 +2,29 @@ package com.hx78.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
+
 /**
  * Created by linming on 2015/8/31 0031.
  */
 public class TextEditor {
     //@Autowired
+    //@Resource
     private SpellChecker spellChecker;
 
-    //@Autowired
-    private String name;
+    @Autowired
+    public TextEditor( SpellChecker spellChecker ) {
+        this.spellChecker = spellChecker;
+    }
 
     public TextEditor() {
     }
 
-    @Autowired
-    public TextEditor( SpellChecker spellChecker, String name ) {
-        this.spellChecker = spellChecker;
-        this.name = name;
+    @PostConstruct
+    public void init(){
+        System.out.println("Bean is going through init.");
     }
 
     //@Autowired
@@ -30,13 +36,12 @@ public class TextEditor {
         return spellChecker;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-
     public void spellCheck() {
         spellChecker.checkSpelling();
     }
 
+    @PreDestroy
+    public void destroy(){
+        System.out.println("Bean will destroy now.");
+    }
 }
